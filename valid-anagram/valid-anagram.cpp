587af1aac4e1e15b94e445c3d14dcc1f602b88f2
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2021-11-12 20:41:36 +0900

    Time: 4 ms (97.33%), Space: 7.2 MB (94.75%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()){
-            return false;
-        }   
+        int a = s.length();
+        int b = t.length();
         
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        if (s != t){
+        if(a!=b)
             return false;
+        
+        int count[256]={0};
+        
+        for(int i=0;i<a;i++)
+            count[s[i]]++;
+        
+        for(int i=0;i<b;i++)
+            count[t[i]]--;
+        
+        for(int i=0;i<256;i++){
+            if(count[i]!=0)
+                return false;
         }
-        else{
-            return true;
-        }
+        return true;
     }
 };
