96cda4151c4f1ab13b78e9c40e643d9f00e884d5
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2021-11-13 21:30:21 +0900

    Time: 0 ms (100.00%), Space: 7.4 MB (10.31%) - LeetHub

---
+++ minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int cnt[2] = {0, };
+        for (auto p : position) 
+            cnt[p % 2]++;
+        return min(cnt[0], cnt[1]);
+    }
+};
