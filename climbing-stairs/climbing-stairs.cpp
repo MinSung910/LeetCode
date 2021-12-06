79a0481df10ca8eafca1f3a76078950cb20134a1
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2021-12-06 17:17:59 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (52.51%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  int climbStairs(int n) {
+        int t[n+1];
+        
+        t[1]=1;
+        if(n>=2) t[2]=2; //if we don't give the condition then it will give runtime error for i/p 1
+        
+        for(int i=3; i<=n; i++)
+            t[i]= t[i-1]+t[i-2];
+        
+        return t[n];
+    }
+};
