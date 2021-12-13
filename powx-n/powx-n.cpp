8269fbf11b33cb7698cc34280d865398cf7a5387
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2021-12-13 21:03:22 +0900

    Time: 0 ms (100.00%), Space: 6 MB (41.80%) - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-    unordered_map<double, int> map;
-    double myPow(double x, int n) {
-        double ans = pow(x, n);
-        return ans;
-    }
+    double myPow(double x, long long n) {
+        if (n == 1 || x == 1 || x == 0) {
+            return x;
+        }
         
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n < 0) {
+            return myPow(1.0 / x, -n);
+        }
+     
+
+        if (n % 2 == 0) {
+            double ret = myPow(x, n / 2);
+            return ret * ret;
+        }
+        else {
+            double ret = myPow(x, (n - 1) / 2);
+            return ret * ret * x;    
+        }
+    }
 };
