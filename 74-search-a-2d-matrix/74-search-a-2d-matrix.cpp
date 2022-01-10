bc23789630cc2349b978efbc4d9d0780cbf3680e
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-01-10 15:57:00 +0900

    Time: 14 ms (5.43%), Space: 9.6 MB (5.72%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto& row : matrix) {
+            for (auto& col : row) {
+                if (col == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
