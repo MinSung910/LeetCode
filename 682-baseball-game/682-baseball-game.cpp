fddae3922c349c5029440f1ac36405c727269487
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-01-28 23:28:40 +0900

    Time: 5 ms (38.32%), Space: 8.5 MB (25.17%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        stack<int> s;
+        
+        for (int i = 0; i < ops.size(); i++) {
+            if (ops[i] == "C") {
+                s.pop();
+            }
+            else if (ops[i] == "D") {
+                s.push(s.top() * 2);
+            }
+            else if (ops[i] == "+") {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.push(a);
+                s.push(a + b);
+            }
+            else {
+                s.push(stoi(ops[i]));
+            }
+        }
+        
+        int ans = 0;
+        while (!s.empty()) {
+            ans += s.top();
+            s.pop();
+        }
+        
+        return ans;
+    }
+};
