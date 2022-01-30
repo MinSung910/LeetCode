8411a227872c8de54e8fc9c13f4e00fa4ce1d113
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-01-30 17:19:25 +0900

    Time: 10 ms (22.97%), Space: 8.8 MB (39.55%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        int row = image.size();
-        int col = image[0].size();
-        
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col / 2; j++) {
-                swap(image[i][j], image[i][col - 1 - j]);
+       for ( auto& r : image ) 
+         {
+            for ( auto& v : r ) 
+            {
+                v ^= 1;
             }
-            for (int j = 0; j < col; j++) {
-                image[i][j] = image[i][j] == 1 ? 0 : 1;
-            }
+           reverse( r.begin(), r.end() );
         }
         
         return image;
