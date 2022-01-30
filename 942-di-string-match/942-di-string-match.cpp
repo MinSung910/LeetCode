1b5961a6e9a429e68678a4f1b8b9d81f060e2f1a
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-01-30 16:35:29 +0900

    Time: 12 ms (16.35%), Space: 9 MB (29.17%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
+        int n = s.length();
         vector<int> ans;
-        int low = 0, high = s.length();
-        vector<bool> visit(s.length(), false);
+        int left = 0, right = n;
         
-        for (char c : s) {
-            if (c == 'I') {
-                visit[low] = true;
-                ans.push_back(low++);
-            }
-            else {
-                visit[high] = true;
-                ans.push_back(high--);
-            }
+        for(int i = 0; i < n; i++)
+        {
+            if(s[i] == 'I')
+                ans.push_back(left++);
+            else
+                ans.push_back(right--);
         }
-        for (int i = 0; i <= s.length(); i++) {
-            if (!visit[i]) ans.push_back(i);
-        }
+        ans.push_back(right);
         return ans;
     }
 };
