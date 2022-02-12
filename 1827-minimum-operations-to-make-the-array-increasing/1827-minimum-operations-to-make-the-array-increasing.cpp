35dabc2c30563815adfb30624a4352c81ebf09b9
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-02-12 16:36:57 +0900

    Time: 12 ms (88.99%), Space: 15.8 MB (78.35%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -4,13 +4,9 @@ public:
         int ans = 0;
         
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] > nums[i - 1] + 1) {
-                continue;
-            }
-            else {
-                int tmp = nums[i - 1] + 1;
-                ans += tmp - nums[i];
-                nums[i] = tmp;
+            if (nums[i] - nums[i - 1] <=  1) {
+                ans += nums[i - 1] + 1 - nums[i];
+                nums[i] = nums[i - 1] + 1;
             }
         }
         return ans;
