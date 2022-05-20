d439a410da30258604f0e9e5549521d8d9edcfc2
MinSung910 <62695426+MinSung910@users.noreply.github.com>
2022-05-20 19:29:42 +0900

    Time: 310 ms (44.42%), Space: 132.4 MB (7.23%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,21 +8,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-	bool isPalindrome(ListNode* head) {
-		vector<int> v;
-
-		while (head) {
-			v.push_back(head->val);
-			head = head->next;
-		}
-
-		for (int i = 0; i < v.size() / 2; i++) {
-			if (v[i] != v[v.size() - 1 - i]) return false;
-		}
-
-		return true;
-	}
+    bool isPalindrome(ListNode* head) {
+        ListNode* tail = head;
+        vector<int> v;
+        
+        while (tail) {
+            v.push_back(tail->val);
+            tail = tail->next;
+        }
+        vector<int> v1 = v;
+        reverse(v.begin(), v.end());
+        
+        for (int i = 0; i < v.size(); i++) {
+            if (v[i] != v1[i]) return false;
+        }
+        return true;
+    }
 };
